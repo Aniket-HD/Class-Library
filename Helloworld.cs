a562b4acfacbea9b9155c8e87b96aaacc523057f
Aniket Deshmukh <aniketdeshmukh1994@gmail.com>
2022-03-18 14:15:51 +0530

    Add project files.

---
+++ Helloworld.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+using System.ServiceModel;
+
+namespace Class_Library
+{
+    public class Helloworld : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            // Obtain the tracing service
+            ITracingService tracingService =
+            (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+
+            // Obtain the execution context from the service provider.  
+            IPluginExecutionContext context = (IPluginExecutionContext)
+                serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+            // The InputParameters collection contains all the data passed in the message request.  
+            if (context.InputParameters.Contains("Target") &&
+                context.InputParameters["Target"] is Entity)
+            {
+                // Obtain the target entity from the input parameters.  
+                Entity entity = (Entity)context.InputParameters["Target"];
+
+                // Obtain the organization service reference which you will need for  
+                // web service calls.  
+                IOrganizationServiceFactory serviceFactory =
+                    (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+                IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+
+                try
+                {
+                    // Plug-in business logic goes here.  
+                    string firstName = String.Empty;
+                    string lastName = String.Empty;
+
+                    //Read Form Attributes
+                    if (entity.Attributes.Contains("firstname"))
+                    {
+                        firstName = entity.Attributes["firstname"].ToString();
+                        firstName = " " + firstName;
+                    }
+                    if (entity.Attributes.Contains("lastname"))
+                    {
+                        lastName = entity.Attributes["lastname"].ToString();
+                        lastName = " " + lastName;
+                    }
+
+                    //Assign data to an attribute
+                    entity.Attributes.Add("description", "Hello," + firstName + lastName);
+                }
+
+                catch (FaultException<OrganizationServiceFault> ex)
+                {
+                    throw new InvalidPluginExecutionException("An error occurred in Helloworld.", ex);
+                }
+
+                //catch (Exception ex)
+                //{
+                //    tracingService.Trace("Helloworld: {0}", ex.ToString());
+                //    throw;
+                //}
+            }
+        }
+    }
+}
